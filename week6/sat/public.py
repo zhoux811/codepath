0f1b73da1af56e567e949fc798412c9f1a148c41
zhoux811 <32617234+zhoux811@users.noreply.github.com>
2021-10-23 14:42:25 -0400

    lect material

---
+++ week6/sat/public.py
@@ -0,0 +1,40 @@
+# Problem 1: Queue using Stack
+
+'''
+# UNDERSTAND
+
+Questions:
+When the queue is empty, what happens if you use peek()?
+
+Notes:
+Return true when the queue is empty.
+
+Test cases:
+(Average):
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+
+Output
+[null, null, null, 1, 1, false]
+
+
+(Edge)
+Input
+["MyQueue", "empty"]
+
+Output
+[true]
+
+
+# MATCH
+
+
+# PLAN
+# IMPLEMENT
+# REVIEW
+# EVALUATE
+
+'''
+
+# Problem 2: Stack using Queue
